d3da079d9025bded51d030fbfc47f8aec093a683
EDUARDO JOSE PIN <eduardo.pin@gmail.com>
2017-07-24 09:58:35 -0300

    Delete Compress Compacta todas as bases de dados de um servidor.sql

--- Compress Compacta todas as bases de dados de um servidor.sql
+++
@@ -1,5 +0,0 @@
-backup database [Virt_DW] to disk = '\\sqlmar\bkp\eventuais\Virt_DW.bak' with compression, stats =1
-backup database [Virt_DW_Stage] to disk = '\\sqlmar\bkp\eventuais\Virt_DW_Stage.bak' with compression, stats =1
-backup database [ReportServer] to disk = '\\sqlmar\bkp\eventuais\ReportServer.bak' with compression, stats =1
-backup database [ReportServerTempDB] to disk = '\\sqlmar\bkp\eventuais\ReportServer.bak' with compression, stats =1
-backup database [msdb] to disk = '\\sqlmar\bkp\eventuais\msdb.bak' with compression, stats =1
